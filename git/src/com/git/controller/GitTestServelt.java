33c3133af4869f1a5c0556437ab18a3a408c3b72
user2 <user2@user2-PC>
2020-04-01 21:02:43 +0900

    #03 controller 출력문 추가 -sw
    확인을 위한 출력문 추가함

--- git/src/com/git/controller/GitTestServelt.java
+++ git/src/com/git/controller/GitTestServelt.java
@@ -27,7 +27,9 @@ public class GitTestServelt extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+		System.out.println("호호호 수정한 내용");
 	}
 
 	/**
